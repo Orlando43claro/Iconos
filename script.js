f7f47768af1f70da5db5b957e480ea100ca2cd1f
developer shop <112266682+Orlando43claro@users.noreply.github.com>
2024-10-08 00:10:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 document.querySelectorAll('.download-btn').forEach(button => {
     button.addEventListener('click', function() {
-        const icon = this.getAttribute('data-icon');
+        const iconName = this.getAttribute('data-icon');
+        const iconUrl = iconName; // Ruta del icono directamente en la misma ruta que HTML
         const progressBar = this.nextElementSibling;
+        const progress = progressBar.querySelector('.progress');
 
         // Mostrar la barra de progreso
         progressBar.style.display = 'block';
 
-        // Animar la barra de progreso
-        let progress = 0;
-        const interval = setInterval(() => {
-            progress += 10;
-            progressBar.firstChild.style.width = progress + '%';
+        // Reiniciar el progreso
+        progress.style.width = '0%';
 
-            if (progress >= 100) {
+        let progressValue = 0;
+
+        // Animar la barra de progreso
+        const interval = setInterval(() => {
+            progressValue += 10;
+            progress.style.width = progressValue + '%';
+
+            if (progressValue >= 100) {
                 clearInterval(interval);
                 // Realiza la descarga una vez que la barra de progreso llegue al 100%
-                downloadIcon(icon);
+                downloadIcon(iconUrl, iconName);
+                // Esconde la barra de progreso después de la descarga
+                setTimeout(() => {
+                    progressBar.style.display = 'none';
+                }, 500);
             }
         }, 100); // Actualiza cada 100 ms
     });
 });
 
-function downloadIcon(icon) {
+function downloadIcon(url, name) {
     const link = document.createElement('a');
-    link.href = `https://i.ibb.co/Kx45TZR/IMG-20240927-185755.png`; // Cambia esta URL por el enlace del icono real
-    link.download = icon;
+    link.href = url;
+    link.download = name;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
